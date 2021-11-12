fbcb8d8735468375f4f1510d6bcecf849017f150
Antonio Membrides Espinosa <tonykssa@gmail.com>
2021-11-12 10:57:06 -0500

    fix traing

--- src/lcs/controllers/LcsController.py
+++ src/lcs/controllers/LcsController.py
@@ -10,29 +10,19 @@ srvMl.setEtl(srvEtl)
 
 @app.route("/api/lcs/generate", methods=["POST"])
 def generate():
-    sample_size = request.form.get("size", 100)
+    sample_size = request.json.get("size", 100)
     result = srvEtl.generate(sample_size)
     resp = make_response(result.to_csv(index=False))
     resp.headers["Content-Disposition"] = "attachment; filename=sample_data_" + str(sample_size) +".csv"
     resp.headers["Content-Type"] = "text/csv"
     return resp
 
-@app.route("/api/lcs/traing2", methods=["POST"])
-def traing2():
-    return jsonify({
-        "cross_validation_scores": 222
-    })
-
-
 @app.route("/api/lcs/traing", methods=["POST"])
 def traing():
-    #print("---------------------------------------------------------")
-    #print(request.files)
-    #f = request.files['archive']
-    #print(">>>>>>>>>>>>>>>>>/api/lcs/traing>>>>>>>>>>>>>>>>>>>>>>>>>")
-    #print(f.filename)
-    #print(">>>>>>>>>>>>>>>>>/api/lcs/traing>>>>>>>>>>>>>>>>>>>>>>>>>")
-    filename = "F:\\sample_data_100.csv"
+    path = os.path.dirname(__file__) + "../../../../data/"
+    filename = path + request.form.get("modelname", 'sample_data_100') + ".csv"
+    filename = os.path.abspath(filename)
+
     cross_validation_scores, cross_validation_score_mean, accuracy_score, precision_score, recall_score, f1_score, classifer_name = \
         srvMl.train(filename)
 
@@ -45,8 +35,6 @@ def traing():
         "f1_score": f1_score,
         "classifer_name": classifer_name
     }
-    print(payload)
-    
     return jsonify(payload)
 
 @app.route("/api/lcs/classify", methods=["POST"])
